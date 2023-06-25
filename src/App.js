26f241f50d9a800ec574ac9cbfcba8390a90e67c
Dimas gapake H <dimasid290503@gmail.com>
2023-06-25 08:18:19 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const  [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json('success');
+    setUsers(users);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.length > 0 &&(
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}, {user.email}</li>
+          ))}
+          
+        </ul>
+      )}
     </div>
   );
 }
